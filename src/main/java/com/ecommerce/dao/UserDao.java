0afa41034fed3790581eb1f11ecf0fc5bcc25863
mrsahilansari0000 <mrsahilansari2003@gmail.com>
2026-07-17 08:43:23 +0530

    pull from my side

--- src/main/java/com/ecommerce/dao/UserDao.java
+++ src/main/java/com/ecommerce/dao/UserDao.java
@@ -12,19 +12,22 @@ public class UserDao {
 
 	public boolean signUp(String username, String email, String password, String role) {
 		String query = CustomerQueries.USER_SIGNUP_QUERY;
-        
-		try  {
+
+		try {
 			Connection connection = DBconfig.getConnection();
 			PreparedStatement pstmt = connection.prepareStatement(query);
+
 			pstmt.setString(1, username);
 			pstmt.setString(2, email);
 			pstmt.setString(3, password);
 			pstmt.setString(4, role);
 
 			int rows = pstmt.executeUpdate();
+
 			return rows > 0;
 
 		} catch (Exception e) {
+
 			e.printStackTrace();
 			return false;
 		}
@@ -32,15 +35,16 @@ public class UserDao {
 
 	public Seller logIn(String email, String password) {
 
-	    try (Connection connection = DBconfig.getConnection();
-	         PreparedStatement pstmt =
-	             connection.prepareStatement(CustomerQueries.USER_LOGIN_QUERY)) {
+		try {
+			Connection connection = DBconfig.getConnection();
+			PreparedStatement pstmt = connection.prepareStatement(CustomerQueries.USER_LOGIN_QUERY);
 
-	        pstmt.setString(1, email);
-	        pstmt.setString(2, password);
+			pstmt.setString(1, email);
+			pstmt.setString(2, password);
 
-	        ResultSet result = pstmt.executeQuery();
+			ResultSet result = pstmt.executeQuery();
 
+<<<<<<< Updated upstream
 	        while (result.next()) {
 	        	int user_id = result.getInt(1);
 	        	String username = result.getString(2);
@@ -60,4 +64,13 @@ public class UserDao {
 	    
 	    return null;
 	}	
+=======
+			return result.next();
+
+		} catch (Exception e) {
+			System.err.println("Error from UserDao: " + e.getMessage());
+			return false;
+		}
+	}
+>>>>>>> Stashed changes
 }
